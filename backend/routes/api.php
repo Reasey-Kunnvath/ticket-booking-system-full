06cf8eb9806de3c077fa34822eee9d1017750de7
Kun Vath <matrixxteame@gmail.com>
2025-03-04 10:19:17 +0700

    setting up api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,8 +12,17 @@ Route::post('/admin/login', [AuthController::class, 'admin_login']);
 Route::post('/event-provider/login', [AuthController::class, 'event_provider_login']);
 
 
-Route::prefix('event-provider')->middleware('auth:sanctum')->group(function () {
-    // only admin ban create event provider ban trov ot
-    // if ot trov jam kae tam ng tov bro
-    Route::post('/', [EventProviderController::class, 'store'])->middleware('role:admin');
+// Route::prefix('event-provider')->middleware('auth:sanctum')->group(function () {
+//     only admin ban create event provider ban trov ot
+//     if ot trov jam kae tam ng tov bro
+//     Route::post('/', [EventProviderController::class, 'store'])->middleware('role:admin');
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    # pkert route api auto (GET, POST, PUT, DELETE,...)
+    Route::apiResource('event-provider', EventProviderController::class)->middleware('role:admin');
+
+    # Sark type "php artisan route:list --path=api" tv khrnh mean endpoint taing os ng hz
+    # Jg somrab controller kron tv pkert function tam endpoint vea muy muy tv (index, store, show, update, destroy)
+    # In summary all api endpoint mean double protection with auth:sanctum and role:admin
 });
